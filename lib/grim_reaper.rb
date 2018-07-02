947513f8c7ec8e5442b53a4c7552382f78446beb
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2018-07-01 23:34:02 -0400

    Initial commit

---
+++ lib/grim_reaper.rb
@@ -0,0 +1,5 @@
+require "grim_reaper/version"
+
+module GrimReaper
+  # Your code goes here...
+end
---
+++ spec/grim_reaper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GrimReaper do
+  it "has a version number" do
+    expect(GrimReaper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
